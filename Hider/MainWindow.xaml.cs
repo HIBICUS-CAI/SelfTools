d042681752f3a11baca2aee6e89b139f86a2577b
Cai Yuanhan <cai_yuanhan@outlook.com>
2024-09-30 15:19:29 +0900

    ものを隠す用のアプリ

---
+++ Hider/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Hider
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            G.Background = new SolidColorBrush(Color.FromRgb(104, 118, 138));
+        }
+    }
+}
