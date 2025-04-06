1ce4bea66e36477e9f2673e9e2fec071f45106c7
Cai Yuanhan <cai_yuanhan@outlook.com>
2025-04-06 17:13:48 +0900

    調整

--- RaspHelper/setup.bat
+++ RaspHelper/setup.bat
@@ -1,13 +1,19 @@
+@echo off
+
 if "%1" NEQ "" goto :setup
 
 set PING_COUNT=
 
 :ping-check
 timeout /t 1 /nobreak >nul
+echo ping checking...
 ping raspberrypi5.local
 if %ERRORLEVEL% NEQ 0 goto :ping-check
 set PING_COUNT=%PING_COUNT%1
+echo %PING_COUNT% / 11111
 if %PING_COUNT% NEQ 11111 goto :ping-check
+
+echo ping finished
 
 :setup
 ssh-keygen -R raspberrypi5.local
