58e7c7dea239e2e48ee38e902bb37afa027a7f86
Cai Yuanhan <cai_yuanhan@outlook.com>
2025-04-06 17:09:21 +0900

    調整

--- RaspHelper/setup_man.sh
+++ RaspHelper/setup_man.sh
@@ -1,10 +1,9 @@
-touch /home/hibic/.zshrc
-
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# touch /home/hibic/.zshrc
+# sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 echo 'export DOTNET_ROOT=$HOME/.dotnet' >> /home/hibic/.zshrc
 echo 'export PATH=$PATH:$HOME/.dotnet' >> /home/hibic/.zshrc
 echo 'alias dmo="python $HOME/code/SelfTools/RaspHelper/device_monitor.py"' >> /home/hibic/.zshrc
-zsh -c 'source /home/hibic/.zshrc'
+source /home/hibic/.zshrc
 
 # sudo chsh -s /usr/bin/zsh
