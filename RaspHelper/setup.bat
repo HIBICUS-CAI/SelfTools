de0383874ea117848becdeddb5facc1de5042a1a
Cai Yuanhan <cai_yuanhan@outlook.com>
2025-04-06 14:10:37 +0900

    更新

--- RaspHelper/setup.bat
+++ RaspHelper/setup.bat
@@ -10,5 +10,6 @@ set PING_COUNT=%PING_COUNT%1
 if %PING_COUNT% NEQ 11111 goto :ping-check
 
 :setup
+ssh-keygen -R raspberrypi5.local
 scp %~dp0setup.sh hibic@raspberrypi5.local:~/
 ssh hibic@raspberrypi5.local mkdir ~/temp; mv -f -v ~/setup.sh ~/temp/; bash ./misc/setup.sh
